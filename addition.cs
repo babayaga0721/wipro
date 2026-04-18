254f2bbb51907eee8b645db26884728dfab9e7bd
Akshay Indrawal <indrawalakshay@gmail.com>
2026-04-18 14:43:58 +0530

    Add files via upload

---
+++ addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int num1, num2, sum;
+        Console.WriteLine("enter first no. :");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter second no. :");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        sum = num1 + num2;
+        Console.WriteLine("Sum = " + sum);
+    }
+}
